09e799423ba3e536036bcea65d110ad6d02f3f70
RagOfJoes <vhsvragojos@gmail.com>
2019-07-04 22:20:16 -0700

    created basic Front-End

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ProofTag Challenge</title>
+    <!-- Font CDN -->
+    <link href="https://fonts.googleapis.com/css?family=DM+Serif+Text&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <!-- BG -->
+        <div class="rowOne"></div>
+
+        <!-- Init. Form -->
+        <div class="rowTwo">
+            <form method="post" action="./prooftag/submit.php">
+                <div class="userInput">
+                    <label for="tagNumber">AUTHENTICATION</label>
+                    <input type="text" name="tagNumber" id="tagNumberInput" placeholder="Tag Number">
+                </div>
+            </form>
+        </div>
+
+        <!-- TODO: rowThree to output required info. or final output -->
+    </div>
+</body>
+
+</html>
